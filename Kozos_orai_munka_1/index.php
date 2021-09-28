f7c1d2bd1048c442ea5a65e13991b4665736f434
Pap <papd@kkszki.hu>
2021-09-28 09:24:52 +0200

    alap layout, header, content + form, footer

--- Kozos_orai_munka_1/index.php
+++ Kozos_orai_munka_1/index.php
@@ -12,7 +12,24 @@
 </head>
 <body>
 
-    <h1 class="text-danger">LOL</h1>
+    <div class="">
+            <h1>Welcome</h1>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+                <div class="col-sm-8 bg-primary">
+                    <p>Content</p>
+                </div>
+                <div class="col-sm-4 bg-dark">
+                    <p>From</p>
+                </div>
+        </div>
+    </div>
+
+    <div class="row">
+            <h2>Footer things</h2>
+    </div>
 
 </body>
 </html>
