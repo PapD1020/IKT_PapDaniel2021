6a00416f1165bb84036976f1720c01d4770240e8
Pap <papd@kkszki.hu>
2021-09-28 11:45:41 +0200

    hoover effect a login gombra, form kijavítása

--- Kozos_orai_munka_1/index.php
+++ Kozos_orai_munka_1/index.php
@@ -13,6 +13,9 @@
     <title>KOM1 - Front page</title>
 </head>
 <body>
+    <?php
+
+    ?>
 
     <div class="container border_bottom">
         <div class="row justify-content-md-center bold_style to_center padding10">
@@ -44,16 +47,16 @@
                     <input type="text" class="form-control" id="userEmailInput" placeholder="email@email.com">
                 </div>
                 <div class="mb-3">
-                    <label for="formGroupExampleInput3" class="form-label bold_style">Password</label>
-                    <input type="text" class="form-control" id="userPasswordInput" placeholder="Password123$">
-                </div>
-                <div class="mb-3">
-                    <label for="formGroupExampleInput4" class="form-label bold_style">Full name</label>
+                    <label for="formGroupExampleInput3" class="form-label bold_style">Full name</label>
                     <input type="text" class="form-control" id="userFullNameInput" placeholder="Full Name">
                 </div>
                 <div class="mb-3">
-                    <label for="formGroupExampleInput5" class="form-label bold_style">Password</label>
+                    <label for="formGroupExampleInput4" class="form-label bold_style">Password</label>
                     <input type="text" class="form-control" id="userPasswordInput" placeholder="Password123$">
+                </div>
+                <div class="mb-3">
+                    <label for="formGroupExampleInput5" class="form-label bold_style">Password again</label>
+                    <input type="text" class="form-control" id="userPasswordInput2" placeholder="Password again">
                 </div>
 
                 <div class="mb-3">
@@ -65,6 +68,7 @@
 
                 <div>
                     <button type="submit" class="btn btn-primary bold_style">Register</button>
+                    <label id="loginButton" class="">Login</label>
                 </div>
             </div>
         </div>
