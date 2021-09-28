347f1a0cab4926c318b6ce9ed6c2fe14cbd9e185
Pap <papd@kkszki.hu>
2021-09-28 08:51:06 +0200

    css, js hozzáadva header-be, css működésének ellenőrzése

--- Kozos_orai_munka_1/index.php
+++ Kozos_orai_munka_1/index.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.bundle.min.js"></script>
+
     <title>KOM1 - Front page</title>
 </head>
 <body>
-    
+
+    <h1 class="text-danger">LOL</h1>
+
 </body>
 </html>
--- Kozos_orai_munka_1/reg.php
+++ Kozos_orai_munka_1/reg.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/bootstrap.min.*">
+    <script src="js/bootstrap.bundle.min.js"></script>
+
     <title>KOM1 - Registration page</title>
 </head>
 <body>
