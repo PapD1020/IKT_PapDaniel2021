cd37d56a27ca38b87091732300ecde7c4aa97594
Pap <papd@kkszki.hu>
2021-10-05 08:52:44 +0200

    2 származtatott osztály létrehozása, új változók létrehozása

--- Kozos_orai_munka_1/classes.php
+++ Kozos_orai_munka_1/classes.php
@@ -2,8 +2,23 @@
     //Ősosztály létrehozása - inheritance
     class Forms{
         protected $userName;
+        protected $password;
+        protected $userNameError;
+        protected $passwordError;
+
+        /*
         protected $email;
         protected $fullName;
-        protected $password;
+        */
+    }
+
+    //Származtatott osztály 01 létrehozása - login űrlap validálása
+    class LoginCheck extends Forms{
+
+    }
+
+    //Származtatott osztály 02 - reg űrlap validálásához
+    class RegCheck extends Forms{
+
     }
 ?>
