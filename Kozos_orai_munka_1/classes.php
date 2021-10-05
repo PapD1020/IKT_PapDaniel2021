fdbc026916718711332b57ef4480da6b1c609570
Pap <papd@kkszki.hu>
2021-10-05 08:26:32 +0200

    classes.php létrehozása, változók létrehozása

---
+++ Kozos_orai_munka_1/classes.php
@@ -0,0 +1,9 @@
+<?php
+    //Ősosztály létrehozása - inheritance
+    class Forms{
+        protected $userName;
+        protected $email;
+        protected $fullName;
+        protected $password;
+    }
+?>
