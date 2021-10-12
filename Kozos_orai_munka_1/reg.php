b055812a476c40da8890c8266bc8ac03d0ce74d3
Pap <papd@kkszki.hu>
2021-10-12 08:22:02 +0200

    pattern added to passwords

--- Kozos_orai_munka_1/reg.php
+++ Kozos_orai_munka_1/reg.php
@@ -2,22 +2,27 @@
             <div class="mb-3">
                     <label for="formGroupExampleInput" class="form-label bold_style">User name</label>
                     <input type="text" class="form-control" id="userNameInput" required placeholder="Unknown User">
+                    <small class="form-text text-muted" name="userInfo1"></small>
                 </div>
                 <div class="mb-3">
                     <label for="formGroupExampleInput2" class="form-label bold_style">Email</label>
                     <input type="email" class="form-control" id="userEmailInput" required placeholder="email@email.com">
+                    <small class="form-text text-muted" name="userInfo1"></small>
                 </div>
                 <div class="mb-3">
                     <label for="formGroupExampleInput3" class="form-label bold_style">Full name</label>
                     <input type="text" class="form-control" id="userFullNameInput" required placeholder="Full Name">
+                    <small class="form-text text-muted" name="userInfo1"></small>
                 </div>
                 <div class="mb-3">
                     <label for="formGroupExampleInput4" class="form-label bold_style">Password</label>
-                    <input type="password" class="form-control" id="userPasswordInput" required placeholder="Password123$">
+                    <input type="password" class="form-control" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}" id="userPasswordInput" required placeholder="Password123$">
+                    <small class="form-text text-muted" name="userInfo1"></small>
                 </div>
                 <div class="mb-3">
                     <label for="formGroupExampleInput5" class="form-label bold_style">Password again</label>
-                    <input type="password" class="form-control" id="userPasswordInput2" required placeholder="Password again">
+                    <input type="password" class="form-control" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}" id="userPasswordInput2" required placeholder="Password again">
+                    <small class="form-text text-muted" name="userInfo1"></small>
                 </div>
 
                 <div>
