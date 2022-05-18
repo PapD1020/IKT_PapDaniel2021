2f07205db69a34851a2ea969d031053394b4978d
PapD1020 <71826326+PapD1020@users.noreply.github.com>
2022-05-18 17:32:50 +0200

    Add files via upload

---
+++ Balkezesek/Balkezesek/Jatekos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    internal class Jatekos
+    {
+        public string nev { get; set; }
+
+        public string elso { get; set; }
+
+        public string utolso { get; set; }
+
+        public double suly { get; set; }
+
+        public double magassag { get; set; }
+
+        public static double InchToCm(double x)
+        {
+            return Math.Round(x * 2.54);
+        }
+
+        public Jatekos(string sor)
+        {
+            string[] jatekosDarabok = sor.Split(';');
+
+            nev = jatekosDarabok[0];
+            elso = jatekosDarabok[1];
+            utolso = jatekosDarabok[2];
+            suly = double.Parse(jatekosDarabok[3]);
+            magassag = double.Parse(jatekosDarabok[4]);
+        }
+    }
+}
---
+++ Balkezesek/Balkezesek/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Balkezesek
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Jatekos> jatekosLista = new List<Jatekos>();
+
+            StreamReader streamReader = new StreamReader("balkezesek.csv");
+
+            streamReader.ReadLine();
+
+            while (!streamReader.EndOfStream)
+            {
+                jatekosLista.Add(new Jatekos(streamReader.ReadLine()));
+            }
+
+            streamReader.Close();
+
+            //Próba kiíratás
+            foreach (var item in jatekosLista)
+            {
+                Console.WriteLine(item.nev + " " + item.elso + " " + item.utolso + " " + item.suly + " " + Jatekos.InchToCm(item.magassag));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
